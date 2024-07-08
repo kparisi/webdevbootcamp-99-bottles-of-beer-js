4531a3b45558b810cdad7bce6a964a8a15c22d77
kparisi <kristenparisi3@gmail.com>
2024-07-08 12:30:29 -0500

    Create script.js
    Added js script

---
+++ script.js
@@ -0,0 +1,7 @@
+for (let i = 99; i >=0; i--){
+    if (i >0){
+        console.log(i + " bottles of beer on the wall, " + i + " bottles of beer. Take one down pass it around, " + i + " bottles of beer on the wall.")
+    } else{
+        console.log("No more bottles of beer on the wall, no more bottles of beer. Go to the store and buy some more, 99 bottles of beer on the wall.");
+    }
+}
